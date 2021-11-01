66e1c09d2118d664d82995cb577c881b403e0e11
Dermot Haughey <hderms@gmail.com>
2021-11-01 16:24:13 -0500

    clarifying comments

--- src/jumphash.rs
+++ src/jumphash.rs
@@ -5,24 +5,33 @@ use std::collections::hash_map::DefaultHasher;
 From the paper "A Fast, Minimal Memory, Consistent Hash Algorithm" by John Lamping, Eric Veach (2014).
 http://arxiv.org/abs/1406.2294
 */
+/// hashes an &str to a u32 which is in the range of 0..buckets
+/// expects a number of buckets greater than zero
 fn jump_hash_from_str(key: &str, buckets: u32) -> u32 {
     assert!(buckets >= 1);
     let mut hasher = DefaultHasher::new();
     key.hash(&mut hasher);
-    let h = hasher.finish();
-    jump_hash_from_u64(h, buckets)
+    let key = hasher.finish();
+    jump_hash_from_u64(key, buckets)
 }
 
+// A constant from a 64 bit linear congruential generator found in the original paper
+// but explained here:
+// https://nuclear.llnl.gov/CNP/rng/rngman/node4.html
+// Jump hash has a 64 bit pseudo-random generator 'embedded' in it
+const SIXTY_FOUR_BIT_LINEAR_CONGRUENTIAL_GENERATOR_A_CONSTANT: i64 = 2862933555777941757;
+/// hashes a u64 to a u32 which is in the range of 0..buckets
+/// expects a number of buckets greater than zero
 fn jump_hash_from_u64(key: u64, buckets: u32) -> u32 {
     assert!(buckets >= 1);
     let mut b: i64 = -1;
     let mut j: i64 = 0;
-    let mut h = key;
+    let mut key = key;
 
     while j < buckets as i64 {
         b = j;
-        h = h.wrapping_mul( 2862933555777941757).wrapping_add(1);
-        j = (((b.wrapping_add(1) as f64) * ((1i64 << 31) as f64)) / ((h >> 33).wrapping_add(1) as f64)) as i64;
+        key = key.wrapping_mul( 2862933555777941757).wrapping_add(1);
+        j = (((b.wrapping_add(1) as f64) * ((1i64 << 31) as f64)) / ((key >> 33).wrapping_add(1) as f64)) as i64;
     }
     b as u32
 }
@@ -44,10 +53,11 @@ mod test {
 
     #[test]
     fn bucket_chosen_always_within_range() {
-        let num_buckets = 100;
-        for i in 0..1_000_000 {
-            let idx = jump_hash_from_str(&i.to_string(), num_buckets);
-            assert!(idx < num_buckets)
+        for num_buckets in 1..500 {
+            for i in 0..100_000 {
+                let idx = jump_hash_from_str(&i.to_string(), num_buckets);
+                assert!(idx < num_buckets)
+            }
         }
     }
     #[test]
@@ -56,18 +66,21 @@ mod test {
         for num_buckets in 5..=1000 {
             let mut key_moved = 0;
             for i in 1..num_keys {
-                let ten_shard = jump_hash_from_str(&i.to_string(), num_buckets);
-                let eleven_shard = jump_hash_from_str(&i.to_string(), num_buckets + 1);
-                if ten_shard != eleven_shard {
-                    if eleven_shard != num_buckets  {
+                let current_shard = jump_hash_from_str(&i.to_string(), num_buckets);
+                let next_shard = jump_hash_from_str(&i.to_string(), num_buckets + 1);
+
+                if current_shard != next_shard {
+                    if next_shard != num_buckets  {
                         panic!("if keys move, we'd expect them to move to new shard, not an existing shard")
                     }
                     key_moved += 1;
                 }
             }
-            println!("keys_moved: {} out of {} which is a proportion {} \n num_keys / num_buckets + 1 = {}", key_moved, num_keys, num_buckets + 1, num_keys / (num_buckets + 1) );
-            println!("actual proportion was {}", key_moved as f64 / num_keys as f64);
-            assert!(((key_moved as f64) < ((num_keys as f64) / (num_buckets as f64 ) * 1.20 )) || (key_moved as f64 / num_keys as f64) < 0.02)
+            //Either we expect the amount of keys moved to the new shard to be consistent with the amount of keys we'd expect to be divided up equally into each shard (with considerable wiggle-room of 15%)
+            let keys_moved_as_acceptable_proportion = (key_moved as f64) < ((num_keys as f64) / (num_buckets as f64 ) * 1.15 );
+            //or we expect the relative number of keys as compared to the total number of keys to be small
+            let keys_moved_as_acceptable_percent_of_total = (key_moved as f64 / num_keys as f64) < 0.02;
+            assert!(keys_moved_as_acceptable_proportion || keys_moved_as_acceptable_percent_of_total)
         }
     }
 
@@ -77,6 +90,7 @@ mod test {
     }
     #[test]
     fn matches_reference_code() {
+        // test case borrowed from https://github.com/dgryski/go-jump/blob/master/jump_test.go
         let cases = vec!(
             TestCase{key: 1, bucket: vec!(0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 17, 17)},
             TestCase{key: 0xdeadbeef, bucket: vec!(0, 1, 2, 3, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 16, 16, 16)},
